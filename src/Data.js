0f8ea675e0f1c15f642e81bea25ea691a6ee20e1
davinci95 <vodinhvan200495@gmail.com>
2023-01-09 15:34:37 +0700

    commit

--- src/Data.js
+++ src/Data.js
@@ -44,11 +44,11 @@ export const awrapper = [
 export const coursesCard = [
    {
       id: 1,
-      cover: "../images/courses/c1.png",
+      cover: "../education/images/courses/c1.png",
       coursesName: "Introducing to Software Engineering",
       courTeacher: [
          {
-            dcover: "./images/back.webp",
+            dcover: "../education/images/back.webp",
             name: "by John Smith",
             totalTime: "50 lectures (190 hrs)",
          },
@@ -58,11 +58,11 @@ export const coursesCard = [
    },
    {
       id: 2,
-      cover: "../images/courses/c2.png",
+      cover: "../education/images/courses/c2.png",
       coursesName: "Enhancing Adobe Photoshop CC 2020 Skills",
       courTeacher: [
          {
-            dcover: "./images/back.webp",
+            dcover: "../education/images/back.webp",
             name: "by Ram Gurung",
             totalTime: "30 lectures (125 hrs)",
          },
@@ -72,11 +72,11 @@ export const coursesCard = [
    },
    {
       id: 3,
-      cover: "../images/courses/c3.png",
+      cover: "../education/images/courses/c3.png",
       coursesName: "HTML, CSS, and Javascript for Web Developers",
       courTeacher: [
          {
-            dcover: "./images/back.webp",
+            dcover: "../education/images/back.webp",
             name: "by Saroj Nepal",
             totalTime: "50 lectures (50 hrs)",
          },
@@ -86,11 +86,11 @@ export const coursesCard = [
    },
    {
       id: 4,
-      cover: "../images/courses/c4.png",
+      cover: "../education/images/courses/c4.png",
       coursesName: "Introducing to Programming with WordPress",
       courTeacher: [
          {
-            dcover: "./images/back.webp",
+            dcover: "../education/images/back.webp",
             name: "by Prabin Thapa Mager",
             totalTime: "20 lectures (20 hrs)",
          },
@@ -100,11 +100,11 @@ export const coursesCard = [
    },
    {
       id: 5,
-      cover: "../images/courses/c5.png",
+      cover: "../education/images/courses/c5.png",
       coursesName: "Introducing to Programming with ReactJS",
       courTeacher: [
          {
-            dcover: "./images/back.webp",
+            dcover: "../education/images/back.webp",
             name: "by Sunil Bishowkaram",
             totalTime: "100 lectures (150 hrs)",
          },
@@ -114,11 +114,11 @@ export const coursesCard = [
    },
    {
       id: 6,
-      cover: "../images/courses/c6.png",
+      cover: "../education/images/courses/c6.png",
       coursesName: "Learn Frontend Programming Language",
       courTeacher: [
          {
-            dcover: "./images/back.webp",
+            dcover: "../education/images/back.webp",
             name: "by Sunil Bishowkaram",
             totalTime: "200 lectures (300 hrs)",
          },
@@ -128,11 +128,11 @@ export const coursesCard = [
    },
    {
       id: 7,
-      cover: "../images/courses/c7.png",
+      cover: "../education/images/courses/c7.png",
       coursesName: "Introducing to with HTML / CSS",
       courTeacher: [
          {
-            dcover: "./images/back.webp",
+            dcover: "../education/images/back.webp",
             name: "by Price",
             totalTime: "20 lectures (50 hrs)",
          },
@@ -142,11 +142,11 @@ export const coursesCard = [
    },
    {
       id: 8,
-      cover: "../images/courses/c8.png",
+      cover: "../education/images/courses/c8.png",
       coursesName: "Introducing to with JAVA",
       courTeacher: [
          {
-            dcover: "./images/back.webp",
+            dcover: "../education/images/back.webp",
             name: "by Petter",
             totalTime: "80 lectures (200 hrs)",
          },
@@ -156,11 +156,11 @@ export const coursesCard = [
    },
    {
       id: 9,
-      cover: "../images/courses/c9.png",
+      cover: "../education/images/courses/c9.png",
       coursesName: "Introducing to with HTML / CSS / JAVASCRIPT",
       courTeacher: [
          {
-            dcover: "./images/back.webp",
+            dcover: "../education/images/back.webp",
             name: "by Petter",
             totalTime: "50 lectures (50 hrs)",
          },
@@ -341,7 +341,7 @@ export const blog = [
       com: "3 COMMENTS ",
       title: "Build your Dream Software & Engineering Career",
       desc: "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
-      cover: "../../images/blog/b1.webp",
+      cover: "../education/images/blog/b1.webp",
    },
    {
       id: 2,
@@ -350,7 +350,7 @@ export const blog = [
       com: "5 COMMENTS ",
       title: "Build your Dream Software & Engineering Career",
       desc: "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
-      cover: "../../images/blog/b2.webp",
+      cover: "../education/images/blog/b2.webp",
    },
    {
       id: 3,
@@ -359,7 +359,7 @@ export const blog = [
       com: "10 COMMENTS ",
       title: "Build your Dream Software & Engineering Career",
       desc: "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
-      cover: "../../images/blog/b3.webp",
+      cover: "../education/images/blog/b3.webp",
    },
    {
       id: 4,
@@ -368,7 +368,7 @@ export const blog = [
       com: "3 COMMENTS ",
       title: "Build your Dream Software & Engineering Career",
       desc: "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
-      cover: "../../images/blog/b4.webp",
+      cover: "../education/images/blog/b4.webp",
    },
    {
       id: 5,
@@ -377,7 +377,7 @@ export const blog = [
       com: "3 COMMENTS ",
       title: "Build your Dream Software & Engineering Career",
       desc: "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
-      cover: "../../images/blog/b5.webp",
+      cover: "../education/images/blog/b5.webp",
    },
    {
       id: 6,
@@ -386,7 +386,7 @@ export const blog = [
       com: "12 COMMENTS ",
       title: "Build your Dream Software & Engineering Career",
       desc: "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
-      cover: "../../images/blog/b6.webp",
+      cover: "../education/images/blog/b6.webp",
    },
 ];
 export const testimonal = [
